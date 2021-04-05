36f612dc1330161d6c2450797a9c3d5ee67cd338
WellingtonFR <gbwellington@hotmail.com>
2021-04-04 22:26:13 -0300

    Adicionado função de inserção de dados em massa na db

--- CursoEntityFrameworkCore/Data/ApplicationContext.cs
+++ CursoEntityFrameworkCore/Data/ApplicationContext.cs
@@ -1,15 +1,21 @@
 ﻿using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CursoEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
-
-    public DbSet<Pedido> Pedidos { get; set; }
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos {get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=efcurso;User Id=sa;Password=#Sudo#4321*;");
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Server=127.0.0.1,1433;Database=efcurso;User Id=sa;Password=#Sudo#4321*;");
         }
 
        protected override void OnModelCreating(ModelBuilder modelBuilder)
--- CursoEntityFrameworkCore/Program.cs
+++ CursoEntityFrameworkCore/Program.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+using System;
 
-namespace CursoEntityFrameworkCore
+namespace CursoEFCore
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InserirDadosEmMassa();
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            var listaDeClientes = new[] {
+              new Cliente
+                {
+                    Nome = "Rafael Almeida",
+                    Telefone = "43998439787",
+                    CEP = "99999900",
+                    Estado = "PR",
+                    Cidade = "Curitiba"
+                },
+               new Cliente
+                {
+                    Nome = "Rafael Almeida 2",
+                    Telefone = "43998439787",
+                    CEP = "99999900",
+                    Estado = "PR",
+                    Cidade = "Curitiba"
+                },
+                new Cliente
+                {
+                    Nome = "Rafael Almeida 3",
+                    Telefone = "43998439787",
+                    CEP = "99999900",
+                    Estado = "PR",
+                    Cidade = "Curitiba"
+                },
+            };
+
+            using var db = new Data.ApplicationContext();
+            //db.AddRange(Produto,cliente);
+            //db.Clientes.AddRange(listaDeClientes);
+            db.Set<Cliente>().AddRange(listaDeClientes);
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total registros: {registros}");
+
+
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true,
+            };
+            using var db = new Data.ApplicationContext();
+            // Diferentes formas de persistir os dados
+            //db.Produtos.Add(produto);
+            //db.Set<Produto>().Add(produto);
+            //db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+            db.SaveChanges();
         }
     }
 }
